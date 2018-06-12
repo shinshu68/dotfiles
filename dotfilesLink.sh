c1e9ddd3298f708de12477dea006971c30469f5c
shinshu68(lab) <r_avsr_r68whitemeet_70@yahoo.co.jp>
2018-06-12 15:59:05 +0900

    [delete] .vimを削除

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,9 +1,7 @@
 #!/bim/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/colors ~/.vim
 ln -sf ~/dotfiles/.git-completion.bash ~/.git-completion.bash
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.inputrc ~/.inputrc
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.git-prompt.sh ~/.git-prompt.sh
-ln -sf ~/dotfiles/bundle/ ~/.vim/bundle
