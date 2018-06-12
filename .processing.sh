e474ecc8477aae6da5a0d1fd22d54809639a8936
shinshu68 <r_avsr_r68whitemeet_70@yahoo.co.jp>
2018-06-12 21:37:55 +0900

    [update] .processing.shをシンボリックリンクに対応

--- .processing.sh
+++ .processing.sh
@@ -1,3 +1,4 @@
 #!bin/bash
-P=`echo $PWD | sed -e 's/.....//' -e 's/\(^.\)/\1:/' -e 's/\(.\)/\U\1/'`
+tmp=`echo pwd -P`
+P=`$tmp | sed -e 's/.....//' -e 's/\(^.\)/\1:/' -e 's/\(.\)/\U\1/'`
 processing-java.exe --sketch=$P $1
