5244bd8b22889b10cf018ff565aa3c99a7b851cd
shinshu68 <r_avsr_r68whitemeet_70@yahoo.co.jp>
2018-06-26 14:39:19 +0900

    [clean] 余計な文を削除

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,5 @@
-#!/bim/sh
+#!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.config/nvim ~/.config/
-ln -sf ~/dotfiles/.git-completion.bash ~/.git-completion.bash
 ln -sf ~/dotfiles/.inputrc ~/.inputrc
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.git-prompt.sh ~/.git-prompt.sh
