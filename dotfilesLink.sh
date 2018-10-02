30444f2571d04e132a404e1b55727936cc860abb
shinshu68 <r_avsr_r68whitemeet_70@yahoo.co.jp>
2018-10-02 09:36:02 +0900

    [clean] sorted dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
-ln -sf $HOME/dotfiles/.config/nvim $HOME/.config/
-ln -sf $HOME/dotfiles/.inputrc $HOME/.inputrc
 ln -sf $HOME/dotfiles/.bashrc $HOME/.bashrc
+ln -sf $HOME/dotfiles/.config/nvim $HOME/.config/
 ln -sf $HOME/dotfiles/.git-completion.bash $HOME/.git-completion.bash
 ln -sf $HOME/dotfiles/.git-prompt.sh $HOME/.git-prompt.sh
+ln -sf $HOME/dotfiles/.inputrc $HOME/.inputrc
+ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
