260122d9980dbee4f5fea10ce4ae2edad8e198c1
shinshu68(lab) <r_avsr_r68whitemeet_70@yahoo.co.jp>
2019-04-23 17:35:57 +0900

    [delete] file remove

--- gitclone.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-git clone git@github.com:toyota-ct-selab/shinshu_works.git
-git clone git@github.com:shinshu68/python.git
-git clone https://git.heroku.com/shinshu-bot-test.git
--- pulseaudio_execute.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /mnt/d/pulseaudio-1.1/bin
-
-while ./pulseaudio.exe > nul 2>&1 || true; do
-    echo 'restart' > /dev/null 2&>1
-done
-
