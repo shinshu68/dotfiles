0a5858a027864fc9d5975ccb6acec94fa9702fc2
shinshu68 <shinshu68@gmail.com>
2019-05-08 20:49:15 +0900

    update config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,13 +1,15 @@
-set PYENV_ROOT ~/.pyenv/bin
-set -x PATH $PYENV_ROOT $PATH
-set -x PATH ~/.local/bin $PATH
-status --is-interactive; and pyenv init - | source
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_CACHE_HOME $HOME/.cache
-set -x PATH "/usr/lib/go-1.10/bin" $PATH
-direnv hook fish | source
 set -x EDITOR nvim
+set -x PATH ~/.local/bin $PATH
 
+direnv hook fish | source
+
+set PYENV_ROOT ~/.pyenv/bin
+set -x PATH $PYENV_ROOT $PATH
+status --is-interactive; and pyenv init - | source
+
+set -x PATH "/usr/lib/go-1.10/bin" $PATH
 set -x GOPATH $HOME/goprojects
 set -x PATH $GOPATH/bin $PATH
 
