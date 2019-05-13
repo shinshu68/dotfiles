99f62b3f00072bd1c976aec90b6f021a341ce66e
shinshu68 <shinshu68@gmail.com>
2019-05-13 16:19:48 +0900

    remove unused path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,13 +1,8 @@
 set -x XDG_CONFIG_HOME $HOME/.config
 set -x XDG_CACHE_HOME $HOME/.cache
 set -x EDITOR nvim
-set -x PATH ~/.local/bin $PATH
 
 direnv hook fish | source
-
-# set -x PYENV_ROOT ~/.pyenv/
-# set -x PATH $PYENV_ROOT $PATH
-# eval (pyenv init - | source)
 
 set -x PATH "/usr/lib/go-1.10/bin" $PATH
 set -x GOPATH $HOME/goprojects
