3ffd618b034eff0aebdf5aa2d45dce373c55257b
shinshu68 <shinshu68@gmail.com>
2019-05-27 00:53:43 +0900

    Update: dotfilesLink.shを新しいディレクトリ構成に対応

--- bin/dotfilesLink.sh
+++ bin/dotfilesLink.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
-ln -sf $HOME/dotfiles/.bashrc $HOME/.bashrc
-ln -sf $HOME/dotfiles/.config/nvim $HOME/.config/
-ln -sf $HOME/dotfiles/.git-completion.bash $HOME/.git-completion.bash
-ln -sf $HOME/dotfiles/.git-prompt.sh $HOME/.git-prompt.sh
-ln -sf $HOME/dotfiles/.inputrc $HOME/.inputrc
-ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
+
+ln -sf $HOME/dotfiles/bash/bashrc                         $HOME/.bashrc
+ln -sf $HOME/dotfiles/config/fish/config.fish             $HOME/.config/fish/config.fish
+ln -sf $HOME/dotfiles/config/fish/functions/cls.fish      $HOME/.config/fish/functions/cls.fish
+ln -sf $HOME/dotfiles/config/fish/functions/hisgre.fish   $HOME/.config/fish/functions/hisgre.fish
+ln -sf $HOME/dotfiles/config/fish/functions/histoday.fish $HOME/.config/fish/functions/histoday.fish
+ln -sf $HOME/dotfiles/config/fish/functions/reporoot.fish $HOME/.config/fish/functions/reporoot.fish
+ln -sf $HOME/dotfiles/config/fish/functions/today.fish    $HOME/.config/fish/functions/today.fish
+ln -sf $HOME/dotfiles/config/flake8                       $HOME/.config/flake8
+ln -sf $HOME/dotfiles/config/gitconfig                    $HOME/.gitconfig
+ln -sf $HOME/dotfiles/config/nvim                         $HOME/.config/nvim
+ln -sf $HOME/dotfiles/tmux/tmux.conf                      $HOME/.tmux.conf
+ln -sf $HOME/dotfiles/vim/vimrc                           $HOME/.vimrc
