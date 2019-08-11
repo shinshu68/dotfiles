0417c71956b2e9d49b97a493de953e9728086bdc
shinshu68 <shinshu68@gmail.com>
2019-08-11 11:38:02 +0900

    Add: ansibleのタグを指定すると実行してくれる関数を追加

---
+++ config/fish/functions/ansible-tag.fish
@@ -0,0 +1,17 @@
+function ansible-run
+    set -l len (count $argv)
+    set -l cmd "ansible-playbook -i 'localhost,' -e '@$HOME/dotfiles/ansible/config.yml' $HOME/dotfiles/ansible/playbook.yml"
+    if test $len -eq 0
+        false
+    else
+        set -l roles (ls $HOME/dotfiles/ansible/roles)
+        for role in $argv
+            contains $role $roles
+            if test $status -eq 0
+                set cmd "$cmd --tags '$role'"
+            end
+        end
+        echo $cmd
+        eval $cmd
+    end
+end
