5c31b07f468d54d7ec221afa3c1a15e3f5640854
shinshu68 <shinshu68@gmail.com>
2019-08-11 11:58:05 +0900

    Fix: 引数がリストになかったときtag無しのコマンドが実行された問題を修正

--- config/fish/functions/ansible-tag.fish
+++ config/fish/functions/ansible-tag.fish
@@ -1,17 +1,21 @@
 function ansible-tag
     set -l len (count $argv)
-    set -l cmd "ansible-playbook -i 'localhost,' -e '@$HOME/dotfiles/ansible/config.yml' $HOME/dotfiles/ansible/playbook.yml"
+    set -l cmd "ansible-playbook -K -i 'localhost,' -e '@$HOME/dotfiles/ansible/config.yml' $HOME/dotfiles/ansible/playbook.yml"
     if test $len -eq 0
-        false
+        true
     else
+        set -l newcmd $cmd
         set -l roles (ls $HOME/dotfiles/ansible/roles)
         for role in $argv
             contains $role $roles
             if test $status -eq 0
-                set cmd "$cmd --tags '$role'"
+                set newcmd "$newcmd --tags '$role'"
             end
         end
-        echo $cmd
-        eval $cmd
+
+        if test $cmd != $newcmd
+            echo $newcmd
+            eval $newcmd
+        end
     end
 end
