0f30c1217c04a93ff2abb0443019b1dbda44ae17
shinshu68 <shinshu68@gmail.com>
2019-08-11 21:02:27 +0900

    :bug: git submoduleの中から親のプロジェクトのルートに戻れなかった問題を修正

--- config/fish/functions/reporoot.fish
+++ config/fish/functions/reporoot.fish
@@ -1,4 +1,9 @@
 function reporoot
+  set -l super (git rev-parse --show-superproject-working-tree)
+  if test $super != "" ^/dev/null >/dev/null
+    cd $super
+    return
+  end
   git rev-parse --show-toplevel ^/dev/null >/dev/null
   if test $status -eq 0
     cd (git rev-parse --show-toplevel)
