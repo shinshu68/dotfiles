30866c617f853a1e1894bedf4eee1229345af983
shinshu68 <shinshu68@gmail.com>
2019-08-13 22:43:41 +0900

    :bug: tmux上でないときは普通に実行するように修正

--- config/fish/functions/contributions.fish
+++ config/fish/functions/contributions.fish
@@ -1,4 +1,10 @@
 function contributions
-    tmux split-window -p 25
-    tmux send-keys "python $HOME/dotfiles/bin/githubcontributions.py" C-m
+    set -l cmd python $HOME/dotfiles/bin/githubcontributions.py
+    set -q TMUX
+    if test $status -eq 0
+        tmux split-window -p 25
+        tmux send-keys "$cmd" C-m
+    else
+        eval $cmd
+    end
 end
