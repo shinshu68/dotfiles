00d6b1f0b0aecfe3fb861beef15ee3896977d874
shinshu68 <shinshu68@gmail.com>
2019-08-14 02:25:25 +0900

    :sparkles: contributionsが自動実行されるように変更

--- config/fish/functions/contributions.fish
+++ config/fish/functions/contributions.fish
@@ -2,8 +2,9 @@ function contributions
     set -l cmd python $HOME/dotfiles/bin/githubcontributions.py
     set -q TMUX
     if test $status -eq 0
+        tmux set-option default-command "$cmd"
         tmux split-window -p 25
-        tmux send-keys "$cmd" C-m
+        # tmux send-keys "$cmd" C-m
     else
         eval $cmd
     end
