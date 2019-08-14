ad81d3cb831f68411190784d2f492e0ac7f847b0
shinshu68 <shinshu68@gmail.com>
2019-08-15 02:54:58 +0900

    :+1: フォント設定用関数を追加
    close #5

---
+++ config/fish/functions/font-setting.fish
@@ -0,0 +1,6 @@
+function font-setting
+    gsettings set org.gnome.desktop.interface monospace-font-name 'Cica 12'
+    gsettings set org.gnome.desktop.interface document-font-name 'Cica 12'
+    gsettings set org.gnome.desktop.interface font-name 'Cica 12'
+end
+
