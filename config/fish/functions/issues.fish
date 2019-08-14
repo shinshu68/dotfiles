aaed04aadaf9cff793e0196d7b369d38e6975973
shinshu68 <shinshu68@gmail.com>
2019-08-15 02:58:50 +0900

    :bug: issuesがgitリポジトリ以下でないときは実行されないように

--- config/fish/functions/issues.fish
+++ config/fish/functions/issues.fish
@@ -1,4 +1,10 @@
 function issues
+    git rev-parse --show-toplevel ^/dev/null >/dev/null
+    if test $status -ne 0
+        false
+        return
+    end
+
     set -l issue (python $HOME/dotfiles/bin/githubissues.py | fzf-tmux -d $FZF_TMUX_HEIGHT)
     if test $status -ne 0
         true
