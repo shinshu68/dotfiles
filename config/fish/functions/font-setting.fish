6ce7e4050c19e04eb12d1021de06842a88874e3f
shinshu68 <shinshu68@gmail.com>
2019-08-15 20:24:53 +0900

    :sparkles: fontのダウンロードを行うように設定

--- config/fish/functions/font-setting.fish
+++ config/fish/functions/font-setting.fish
@@ -1,6 +1,15 @@
 function font-setting
+    cd ~/Downloads
+    curl -O https://github.com/miiton/Cica/releases/download/v5.0.1/Cica_v5.0.1_with_emoji.zip
+    unzip Cica_v5.0.1_with_emoji.zip
+    cp -f *.ttf ~/.local/share/fonts
+
+    fc-cache -fv
+
     gsettings set org.gnome.desktop.interface monospace-font-name 'Cica 12'
     gsettings set org.gnome.desktop.interface document-font-name 'Cica 12'
     gsettings set org.gnome.desktop.interface font-name 'Cica 12'
+
+    cd
 end
 
