acc3b80d9e26dce054f64dfeec317eecaf344dd7
shinshu68 <shinshu68@gmail.com>
2019-08-24 20:41:00 +0900

    :fire: コメントアウトしていた部分を削除

--- config/fish/functions/contributions.fish
+++ config/fish/functions/contributions.fish
@@ -3,21 +3,7 @@ function contributions
     set -q TMUX
     if test $status -eq 0
         tmux set-option default-command "$cmd"
-        # set -l panes (string split ' ' (tmux list-panes | sed -e 's/:.*%[0-9]\+//g'))
-        # set -l active_pane 0
-        # set -l tmp ''
-        # for pane in $panes
-        #     if test $tmp = '(active)'
-        #         break
-        #     end
-        #     set active_pane $tmp
-        #     set tmp $pane
-        # end
-        # tmux select-pane -t 0
         tmux split-window -l 10
-        # tmux split-window -l (expr $LINES - 10)
-        # tmux swap-pane -s 1 -t 0
-        # tmux select-pane -t 0
         tmux set-option default-command ""
     else
         eval $cmd
