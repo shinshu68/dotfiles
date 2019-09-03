484af6c0609f65de4b8b6e5e67a1b7dd712ac2f4
shinshu68 <shinshu68@gmail.com>
2019-09-03 12:28:24 +0900

    :sparkles: resourceに補完を追加 allで全てのsymlinkをsourceするように変更

--- config/fish/functions/resource.fish
+++ config/fish/functions/resource.fish
@@ -1,17 +1,44 @@
 function resource
-    set -l config_path $XDG_CONFIG_HOME/fish
-    for file in (ls $config_path)
-        if islink $config_path/$file
-            source $config_path/$file
-            echo $file
-        end
+    if test (count $argv) -ne 1
+        false
+        return
     end
 
+    set -l config_path $XDG_CONFIG_HOME/fish
     set -l function_path $XDG_CONFIG_HOME/fish/functions
-    for file in (ls $function_path)
-        if islink $function_path/$file
-            source $function_path/$file
-            echo $file
+
+    if test $argv = 'all'
+        for file in (ls $config_path)
+            if islink $config_path/$file
+                source $config_path/$file
+                echo $file
+            end
         end
+
+        for file in (ls $function_path)
+            if test $file = 'resource.fish'
+                continue
+            end
+
+            if islink $function_path/$file
+                source $function_path/$file
+                echo $file
+            end
+        end
+    else
+        set -l file (find $XDG_CONFIG_HOME/fish | grep '\.fish$' | grep -F "$argv")
+        source $file
     end
 end
+
+set -l link_files 'all'
+for file in (find $XDG_CONFIG_HOME/fish | grep '\.fish$')
+    if islink $file
+        set link_files $link_files (basename $file)
+    end
+end
+
+# リストを文字列に結合する
+set link_files (echo $link_files | string join ' ')
+
+complete --command resource -r -a $link_files
