92f3748dc53e7aa9a9e45e850071830de235727e
shinshu68 <shinshu68@gmail.com>
2019-09-03 11:25:23 +0900

    :recycle: 少し修正

--- config/fish/functions/resource.fish
+++ config/fish/functions/resource.fish
@@ -1,8 +1,7 @@
 function resource
     set -l config_path $XDG_CONFIG_HOME/fish
     for file in (ls $config_path)
-        islink $config_path/$file
-        if test $status -eq 0
+        if islink $config_path/$file
             source $config_path/$file
             echo $file
         end
@@ -10,8 +9,7 @@ function resource
 
     set -l function_path $XDG_CONFIG_HOME/fish/functions
     for file in (ls $function_path)
-        islink $function_path/$file
-        if test $status -eq 0
+        if islink $function_path/$file
             source $function_path/$file
             echo $file
         end
