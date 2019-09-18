252e2a3bdbac27e8a98092f755f4ee0d29937011
shinshu68 <shinshu68@gmail.com>
2019-09-18 14:22:56 +0900

    :hammer: lint

--- config/fish/functions/reporoot.fish
+++ config/fish/functions/reporoot.fish
@@ -1,13 +1,13 @@
 function reporoot
-  set -l super (git rev-parse --show-superproject-working-tree)
-  if test $super != "" ^/dev/null >/dev/null
-    cd $super
-    return
-  end
-  git rev-parse --show-toplevel ^/dev/null >/dev/null
-  if test $status -eq 0
-    cd (git rev-parse --show-toplevel)
-  else 
-    false
-  end
+    set -l super (git rev-parse --show-superproject-working-tree)
+    if test $super != "" ^/dev/null >/dev/null
+        cd $super
+        return
+    end
+    git rev-parse --show-toplevel ^/dev/null >/dev/null
+    if test $status -eq 0
+        cd (git rev-parse --show-toplevel)
+    else 
+        false
+    end
 end
