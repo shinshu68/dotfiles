db942d95cfc6a6274264dee364ee6833a543ff49
shinshu68 <shinshu68@gmail.com>
2019-10-07 16:06:11 +0900

    :sparkles: entrypoint.shを使ったユーザの追加設定を記述

---
+++ docker/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+USER_ID=${UID}
+GROUP_ID=${GID}
+
+echo "Starting with UID : $USER_ID, GID: $GROUP_ID $@"
+useradd -u $USER_ID -o -m -s /usr/bin/fish shinshu
+export HOME=/home/shinshu
+
+exec /usr/sbin/gosu shinshu fish
