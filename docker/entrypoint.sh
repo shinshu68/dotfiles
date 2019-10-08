1ec7d85c0ad8e0825e020ce7fb787b4cb6337e97
shinshu68 <shinshu68@gmail.com>
2019-10-08 16:34:56 +0900

    :bug: pipがなかったので入れる
    ついでにgosuのやつを戻す

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,3 +7,5 @@ echo "Starting with UID : $USER_ID, GID: $GROUP_ID $@"
 useradd -u $USER_ID -o -m -s /usr/bin/fish shinshu
 export HOME=/home/shinshu
 gpasswd -a shinshu docker
+
+exec /usr/sbin/gosu shinshu "$@"
