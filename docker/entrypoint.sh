316ce72d9d24f47093471c52d973491df51bf08e
shinshu68 <shinshu68@gmail.com>
2019-10-08 12:24:45 +0900

    :bug: imageの設定を修正

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,5 +6,6 @@ GROUP_ID=${GID}
 echo "Starting with UID : $USER_ID, GID: $GROUP_ID $@"
 useradd -u $USER_ID -o -m -s /usr/bin/fish shinshu
 export HOME=/home/shinshu
+gpasswd -a shinshu docker
 
 exec /usr/sbin/gosu shinshu "$@"
