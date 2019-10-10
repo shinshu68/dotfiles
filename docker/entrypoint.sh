5d7293a8f83b81f27bf5782af61ca45ac2a6269b
shinshu68 <shinshu68@gmail.com>
2019-10-10 10:44:17 +0900

    Revert ":recycle: 環境変数の引継ぎ方法を変更"
    This reverts commit b5cc1222e47ea2811f84741abe06fa366e08289e.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+USER_ID=${UID}
+GROUP_ID=${GID}
+
 echo "Starting with UID : $USER_ID, GID: $GROUP_ID $@"
 useradd -u $USER_ID -o -m -s /usr/bin/fish shinshu
 export HOME=/home/shinshu
