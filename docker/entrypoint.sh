179e09467aef9b8b6bc1a4b408c9487e85c6fe73
shinshu68 <shinshu68@gmail.com>
2019-10-10 04:14:30 +0000

    :sparkles: upするときにユーザ名を指定できるように変更

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,10 +2,11 @@
 
 USER_ID=${UID}
 GROUP_ID=${GID}
+USER_NAME=${USERNAME}
 
-echo "Starting with UID : $USER_ID, GID: $GROUP_ID $@"
-useradd -u $USER_ID -o -m -s /usr/bin/fish shinshu
-export HOME=/home/shinshu
-gpasswd -a shinshu docker
+echo "Starting with UID : $USER_ID, GID: $GROUP_ID $USER_NAME $@"
+useradd -u $USER_ID -o -m -s /usr/bin/fish $USER_NAME
+export HOME=/home/$USER_NAME
+gpasswd -a $USER_NAME docker
 
-exec /usr/sbin/gosu shinshu "$@"
+exec /usr/sbin/gosu $USER_NAME "$@"
