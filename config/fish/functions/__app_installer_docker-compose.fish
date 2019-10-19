e79a60f2d12b9b56b29669a26cf6462420315733
shinshu68 <shinshu68@gmail.com>
2019-10-19 18:02:12 +0900

    :sparkles: docker-composeをインストールする関数を追加

---
+++ config/fish/functions/__app_installer_docker-compose.fish
@@ -0,0 +1,4 @@
+function __app_installer_docker-compose
+    curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s-uname -m` -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+end
