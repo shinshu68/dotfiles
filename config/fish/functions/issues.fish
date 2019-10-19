804383e4c6b0f7837781bb679ce82e8817fd2420
shinshu68 <shinshu68@gmail.com>
2019-10-19 22:59:32 +0900

    :bug: pythonをpython3に変更

--- config/fish/functions/issues.fish
+++ config/fish/functions/issues.fish
@@ -5,11 +5,11 @@ function issues
         return
     end
 
-    set -l issue (python $HOME/dotfiles/bin/githubissues.py | fzf-tmux -d $FZF_TMUX_HEIGHT)
+    set -l issue (python3 $HOME/dotfiles/bin/githubissues.py | fzf-tmux -d $FZF_TMUX_HEIGHT)
     if test $status -ne 0
         true
         return
     end
     set -l issue_number (echo $issue | awk '{print $1}')
-    python $HOME/dotfiles/bin/githubissues.py $issue_number
+    python3 $HOME/dotfiles/bin/githubissues.py $issue_number
 end
