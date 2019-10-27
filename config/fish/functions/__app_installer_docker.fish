0ed034722e5ae525705f4b6dc48fcf96c9256e4f
shinshu68 <shinshu68@gmail.com>
2019-10-27 16:37:54 +0900

    :sparkles: dockerがあったら動作しないように設定

--- config/fish/functions/__app_installer_docker.fish
+++ config/fish/functions/__app_installer_docker.fish
@@ -1,7 +1,9 @@
 function __app_installer_docker
-    sudo apt-get install apt-transport-https ca-certificates
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-    sudo apt-get install docker-ce
-    sudo systemctl start docker
+    if not test -x /usr/bin/docker
+        sudo apt-get install apt-transport-https ca-certificates
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+        sudo apt-get install docker-ce
+        sudo systemctl start docker
+    end
 end
