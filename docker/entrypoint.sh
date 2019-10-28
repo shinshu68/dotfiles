75afd24f384df8a2b96c758b93ab98166db86a63
shinshu68 <shinshu68@gmail.com>
2019-10-28 12:27:02 +0900

    :bug: userが無限に追加されないように設定

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,8 +5,12 @@ GROUP_ID=${GID}
 USER_NAME=${USERNAME}
 
 echo "Starting with UID : $USER_ID, GID: $GROUP_ID $USER_NAME $@"
-useradd -u $USER_ID -o -m -s /usr/bin/fish $USER_NAME
-export HOME=/home/$USER_NAME
-gpasswd -a $USER_NAME docker
+
+users | grep $USER_NAME >/dev/null 2>&1
+if [ $? != 0 ]; then
+    useradd -u $USER_ID -o -m -s /usr/bin/fish $USER_NAME
+    export HOME=/home/$USER_NAME
+    gpasswd -a $USER_NAME docker
+fi
 
 exec /usr/sbin/gosu $USER_NAME "$@"
