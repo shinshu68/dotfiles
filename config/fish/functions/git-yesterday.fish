9fb61ebfdc84089ac5fadc0670cd5e0ffe11abee
shinshu68 <shinshu68@gmail.com>
2019-10-30 01:07:41 +0900

    :sparkles: 前日のコミットがなかった時に行うように変更

--- config/fish/functions/git-yesterday.fish
+++ config/fish/functions/git-yesterday.fish
@@ -3,6 +3,12 @@ function git-yesterday
     if test $status -ne 0
         false
     end
+
+    git log | grep "^Date:" | grep (date --date "-1 days" +"%a %b %d") 2>/dev/null >/dev/null
+    if test $status -eq 0
+        return
+    end
+
     set -l date (before-date-git-style 1)
     git rebase -i HEAD~1
     git commit --amend --date="$date"
