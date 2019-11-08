8a601499c2e0454fce2c420269aa835746a34eec
shinshu68 <shinshu68@gmail.com>
2019-11-08 17:19:36 +0900

    :sparkles: 結果を内部変数に格納して最後にechoで吐き出すようにした

--- config/fish/functions/ignore-tree.fish
+++ config/fish/functions/ignore-tree.fish
@@ -9,6 +9,7 @@ function ignore-tree
     set -l files (tree -afCI "*.git*" . | awk '{print $NF}')[1..-3] # 最後の2行は使用しないので消す
     set -l ignore_files (git ls-files -i -o --exclude-standard)
 
+    set -l out ""
     for i in (seq (count $files))
         set -l flag 0
         for ignore_file in $ignore_files
@@ -18,11 +19,14 @@ function ignore-tree
         end
 
         if test $flag -eq 0
-            echo $tree[$i]
+            # echo $tree[$i]
+            set out $out$tree[$i]'\n'
         else
-            echo -e $tree[$i] '(\x1b[38;2;255;0;0mignore\x1b[0m)'
+            # echo -e $tree[$i] '(\x1b[38;2;255;0;0mignore\x1b[0m)'
+            set out $out$tree[$i]'(\x1b[38;2;255;0;0mignore\x1b[0m)\n'
         end
     end
+    echo -e $out
     echo $tree[-2]
     echo $tree[-1]
 end
