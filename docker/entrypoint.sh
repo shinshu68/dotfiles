1c968c0fc22085ae6cde24646f454b91362637ce
shinshu68 <shinshu68@gmail.com>
2019-11-25 16:33:47 +0900

    :bug: $HOMEの設定方法を修正

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,7 +9,8 @@ echo "Starting with UID : $USER_ID, GID: $GROUP_ID $USER_NAME $@"
 users | grep $USER_NAME >/dev/null 2>&1
 if [ $? != 0 ]; then
     useradd -u $USER_ID -o -m -s /usr/bin/fish $USER_NAME
-    export HOME=/$USER_NAME
+    usermod -d /$USER_NAME $USER_NAME
+    # export HOME=/$USER_NAME
     gpasswd -a $USER_NAME docker
     gpasswd -a $USER_NAME sudo
     echo "${USER_NAME}:${USER_NAME}" | chpasswd
