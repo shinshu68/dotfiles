d1f5792d17eba99de0f626ec55e9453c9d0ace50
shinshu68 <shinshu68@gmail.com>
2020-01-12 21:25:12 +0900

    :recycle: issuesコマンドを修正

--- config/fish/functions/issues.fish
+++ config/fish/functions/issues.fish
@@ -3,12 +3,12 @@ function issues
         false
         return
     end
-
-    set -l issue (python3 $HOME/dotfiles/bin/githubissues.py | fzf-tmux -d $FZF_TMUX_HEIGHT)
+    python3 $HOME/dotfiles/bin/get_github_issues.py
+    set -l issue (python3 $HOME/dotfiles/bin/print_issues.py | fzf-tmux -d $FZF_TMUX_HEIGHT)
     if test $status -ne 0
         true
         return
     end
     set -l issue_number (echo $issue | awk '{print $1}')
-    python3 $HOME/dotfiles/bin/githubissues.py $issue_number
+    python3 $HOME/dotfiles/bin/print_issues.py $issue_number
 end
