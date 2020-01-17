f46eba28fbb9a3dc1fab6bcf00c089fdb8512e50
shinshu68 <shinshu68@gmail.com>
2020-01-18 00:33:27 +0900

    :sparkles: 引数で数字を与えられるようにする

--- config/fish/functions/git-yesterday.fish
+++ config/fish/functions/git-yesterday.fish
@@ -4,13 +4,18 @@ function git-yesterday
         return
     end
 
+    set -l before_date 1
+    if test (count $argv) -eq 1
+        set before_date $argv
+    end
+
     git log | grep "^Date:" | grep (date --date "-1 days" +"%a %b %d") 2>/dev/null >/dev/null
     if test $status -eq 0
         echo "Yesterday commit exists."
         return
     end
 
-    set -l date (before-date-git-style 1)
+    set -l date (before-date-git-style $before_date)
     git rebase -i HEAD~1
     git commit --amend --date="$date"
     git rebase --continue
