9b11ab9cf67521f61e6b418a152554e2f0200698
shinshu68 <shinshu68@gmail.com>
2020-01-23 10:55:12 +0900

    :thumbsup: reporootの文字列を返すコマンドを追加

---
+++ config/fish/functions/__reporoot_str.fish
@@ -0,0 +1,15 @@
+function __reporoot_str
+    set -l super (git rev-parse --show-superproject-working-tree 2>/dev/null)
+    if test $status -eq 0 -a "$super" != ""
+        echo $super
+        return
+    end
+
+    set -l reporoot (git rev-parse --show-toplevel 2>/dev/null)
+    if test $status -eq 0
+        echo $reporoot
+        return
+    end
+
+    false
+end
