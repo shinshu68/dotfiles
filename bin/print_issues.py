c1bf0a0c6cc6448d28256e97853d1f89cc3c18a5
shinshu68 <shinshu68@gmail.com>
2020-01-27 23:00:49 +0900

    :bug: .issue_dataがあってもそのrepoのissueが登録されていないときの挙動を修正

--- bin/print_issues.py
+++ bin/print_issues.py
@@ -36,17 +36,12 @@ def get_remote_repo_name():
     return origin_url[len(host):-len(ext)]
 
 
-def print_issue_titles():
-    home = os.getenv('HOME')
-    repo_name = get_remote_repo_name()
-    with open(f'{home}/dotfiles/bin/.issue_data') as f:
-        data = json.load(f)[repo_name]
-
+def print_issue_titles(data):
     for num, value in data.items():
         print(f'{num:>3}', value['title'])
 
 
-def show_detail(num):
+def show_detail(data, num):
     home = os.getenv('HOME')
     repo_name = get_remote_repo_name()
     issue = None
@@ -65,10 +60,19 @@ def show_detail(num):
 
 if __name__ == '__main__':
     home = os.getenv('HOME')
+    repo_name = get_remote_repo_name()
+
     if not os.path.exists(f'{home}/dotfiles/bin/.issue_data'):
         exit()
 
+    with open(f'{home}/dotfiles/bin/.issue_data') as f:
+        issue_data = json.load(f)
+        if repo_name not in issue_data:
+            exit()
+
+        repo_issues = issue_data[repo_name]
+
     if len(sys.argv) == 1:
-        print_issue_titles()
+        print_issue_titles(repo_issues)
     else:
-        show_detail(sys.argv[1])
+        show_detail(repo_issues, sys.argv[1])
