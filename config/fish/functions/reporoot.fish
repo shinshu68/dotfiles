eb9f39a44d780ac7731542275ff173cf097916b4
shinshu68 <shinshu68@gmail.com>
2020-02-10 09:00:42 +0900

    :sparkles: reporootで使用する内部関数を取り込む

--- config/fish/functions/reporoot.fish
+++ config/fish/functions/reporoot.fish
@@ -1,3 +1,19 @@
+function __reporoot_str
+    set -l super (git rev-parse --show-superproject-working-tree 2>/dev/null)
+    if test $status -eq 0 -a "$super" != ""
+        echo $super
+        return
+    end
+
+    set -l reporoot (git rev-parse --show-toplevel 2>/dev/null)
+    if test $status -eq 0
+        echo $reporoot
+        return
+    end
+
+    false
+end
+
 function reporoot
     if not is-inside-git-dir
         false
