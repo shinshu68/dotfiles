870e6c2bf2d404f68a2e1117f4effc6713834c3c
shinshu68 <shinshu68@gmail.com>
2020-02-25 19:57:05 +0900

    :sparkles: 作成したモジュールを使用

--- bin/get_github_issues.py
+++ bin/get_github_issues.py
@@ -1,40 +1,8 @@
 from github import Github
 import click
-import git
 import json
 import os
-import subprocess
-
-
-def get_status():
-    return subprocess.run('git rev-parse --show-toplevel',
-                          shell=True,
-                          stdout=subprocess.PIPE,
-                          stderr=subprocess.PIPE)
-
-
-def is_inside_git_dir():
-    if get_status().returncode == 0:
-        return True
-    else:
-        return False
-
-
-def get_remote_repo_name():
-    if not is_inside_git_dir():
-        exit()
-
-    local_repo = git.Repo(get_status().stdout.decode('utf-8').strip())
-
-    host = 'git@github.com:'
-    ext = '.git'
-
-    origin_url = local_repo.remotes.origin.url
-
-    if origin_url[:5] == 'https':
-        host = 'https://github.com/'
-
-    return origin_url[len(host):-len(ext)]
+from modules.issues import is_inside_git_dir, get_remote_repo_name
 
 
 def get_remote_repo(repo_name):
