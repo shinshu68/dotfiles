1ed66a58a3bcb266dd95946fb5b96ebc13379bc5
shinshu68 <shinshu68@gmail.com>
2020-02-27 22:20:31 +0900

    :sparkles: ファイル名の変更に追従

--- config/fish/functions/contributions.fish
+++ config/fish/functions/contributions.fish
@@ -1,5 +1,5 @@
 function contributions
-    set -l cmd python3 $HOME/dotfiles/bin/githubcontributions.py $argv
+    set -l cmd python3 $HOME/dotfiles/bin/github_contributions.py $argv
     if set -q TMUX
         tmux set-option default-command "$cmd"
         tmux split-window -l 10
